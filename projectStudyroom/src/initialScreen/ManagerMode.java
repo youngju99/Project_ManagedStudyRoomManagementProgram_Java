444d17d29e9ef29f294a3c97c44cb1bfd1d474cc
CHOGYUWAN <95117335+CHOGYUWAN@users.noreply.github.com>
2022-11-07 23:27:06 +0900

    Update ManagerMode.java

--- projectStudyroom/src/initialScreen/ManagerMode.java
+++ projectStudyroom/src/initialScreen/ManagerMode.java
@@ -1,6 +1,8 @@
 // 기본 틀입니다
 // 변수들은 코드 병합 및 1차 수정을 담당하시는 규완님께서 필요한 조건에 따라 추가해주시면 감사하겠습니다
 
+// 회원정보 수정부분 추가하였습니다. 2022/11/07
+
 package initialScreen;
 
 /*
@@ -14,22 +16,25 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.text.ParseException;
 
 import db.MySqlConnect;
 import studyRoom.seat.*;
 import studyRoom.sms.SmsMain;
 import studyRoom.pay.PayMain;
 import studyRoom.pay.UserMain;
+import studyRoom.user_editor.EditorMain;
 
 public class ManagerMode {
 
-	public static void managerMode()  throws IOException, SQLException {
+	public static void managerMode()  throws IOException, SQLException, ParseException {
 		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
 		
 		MySqlConnect mySqlConnect=new MySqlConnect();
 		PayMain payMain = new PayMain();
 		UserMain userMain = new UserMain();
 		SmsMain smsMain=new SmsMain(); // SmsMain객체 생성. 이를 이용해 smsExe 실행 
+		EditorMain editorMain = new EditorMain();
 		
 
 		Connection conn=null; 
@@ -42,7 +47,7 @@ public class ManagerMode {
 		
 		while(true) {
 			System.out.println("========================================================================================================================");
-			System.out.println(" 0: 좌석 추가 | 1: 결제 | 2: 신규 회원 등록 | 3: 좌석 이용 종료 | 4: 상벌점 관리 | 5: SMS 전송 | 6: 작업 종료");
+			System.out.println(" 0: 좌석 추가 | 1: 결제 | 2: 신규 회원 등록 | 3: 좌석 이용 종료 | 4: 상벌점 관리 | 5: SMS 전송 | 6: 회원정보 변경 | 7: 작업 종료");
 			System.out.println("========================================================================================================================");
 			System.out.print("실행할 작업>> ");
 			mode=Integer.parseInt(br.readLine());
@@ -60,12 +65,12 @@ public class ManagerMode {
 				// 좌석 이용 종료
 				ClearSeat.showClearSeatMenu(conn);
 			} else if(mode==4) {
-
+				
 			} else if(mode==5) {
-				
 				smsMain.main(conn);
-				
 			} else if(mode==6) {
+				editorMain.editorUser(conn);
+			} else if(mode==7) {
 				System.out.println("시스템을 종료합니다.\n");
 				mySqlConnect.disconnect(conn);
 				break;
