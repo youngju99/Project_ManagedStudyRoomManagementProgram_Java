e74fa1d55f5b48a0ef50dcfcf371968202393109
CHOGYUWAN <95117335+CHOGYUWAN@users.noreply.github.com>
2022-11-08 12:45:10 +0900

    Update UserMain.java

--- projectStudyroom/src/studyRoom/pay/UserMain.java
+++ projectStudyroom/src/studyRoom/pay/UserMain.java
@@ -7,34 +7,29 @@ package studyRoom.pay;
  * 
  * @re : 
  * @date : 2022/11/07
+ * 
+ * @re : 경고 제거
+ * @date : 2022/11/08
  */
 
 import java.io.*;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.Timestamp;
-import java.util.Calendar;
-import java.util.Scanner;
 
 public class UserMain {
 	public static void inputUser(Connection conn) throws IOException {
 		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-		Scanner sc = new Scanner(System.in);
-		Timestamp ts = new Timestamp(System.currentTimeMillis()); // timestamp
-		Timestamp ts2 = new Timestamp(System.currentTimeMillis());
-		Calendar cal = Calendar.getInstance();
 		PreparedStatement tmt=null;
 		
-		UserSeatSql userinput = new UserSeatSql();
+		UserPaySql userinput = new UserPaySql();
 		
-		int userID = 0;
 		
 		System.out.println("이름을 입력하세요 >> ");
 		String userName = br.readLine();
 		System.out.println("학교를 입력하세요 >> ");
 		String userSchool = br.readLine();
 		System.out.println("학년을 입력하세요 >> ");
-		int userGrade = sc.nextInt();
+		int userGrade = Integer.parseInt(br.readLine());
 		System.out.println("회원 핸드폰번호를 입력하세요 >> ");
 		String userMobile = br.readLine();
 		System.out.println("부모님 핸드폰번호를 입력하세요 >> ");
@@ -45,9 +40,8 @@ public class UserMain {
 		// usertable에 추가
 		UserSeatHistory user_input = new UserSeatHistory(userName, userSchool, userGrade, 
 				userMobile, parentMobile, sms);
-		int insertCount_user = userinput.addUser(conn, tmt, user_input);
-
-
+		userinput.addUser(conn, tmt, user_input);
+		
 		System.out.println("등록되었습니다.");
 	}
 }
