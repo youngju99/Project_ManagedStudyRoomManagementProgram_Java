e5578e4030fa4d756a00ab1e7ff9888a20f75087
CHOGYUWAN <95117335+CHOGYUWAN@users.noreply.github.com>
2022-11-08 12:43:14 +0900

    Update PayMain.java

--- projectStudyroom/src/studyRoom/pay/PayMain.java
+++ projectStudyroom/src/studyRoom/pay/PayMain.java
@@ -7,6 +7,9 @@ package studyRoom.pay;
  * 
  * @re : 
  * @date : 2022/11/07
+ * 
+ * @re : 경고제거
+ * @date : 2022/11/07
  */
 
 import java.io.BufferedReader;
@@ -17,13 +20,11 @@ import java.sql.PreparedStatement;
 import java.sql.Timestamp;
 import java.util.Calendar;
 import java.util.List;
-import java.util.Scanner;
 
 public class PayMain {
 	
 	public static void pay(Connection conn) throws IOException {
 		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-		Scanner sc = new Scanner(System.in);
 		Timestamp ts = new Timestamp(System.currentTimeMillis()); // timestamp
 		Timestamp ts2 = new Timestamp(System.currentTimeMillis());
 		Calendar cal = Calendar.getInstance();
@@ -51,14 +52,14 @@ public class PayMain {
 			
 			System.out.println();
 			System.out.println("결제할 회원의 아이디를 입력하세요 >> ");
-			userID = sc.nextInt();
+			userID = Integer.parseInt(br.readLine());
 			
 			// blacklist인지 판단
-			if (userseat.blacklist(conn, tmt, userID)) {
+			if (UserPaySql.blacklist(conn, tmt, userID)) {
 				// 등록일 수 입력
 				startDate = ts;
 				System.out.println("등록일수를 입력하세요(일단위) >>  ");
-				date = sc.nextInt();
+				date = Integer.parseInt(br.readLine());
 				cal.setTime(ts2);
 				cal.add(Calendar.DATE, date);
 				ts2.setTime(cal.getTime().getTime());
@@ -69,15 +70,15 @@ public class PayMain {
 				// 1일 15000원으로 계산해 결제날자랑해서  pay db에 저장
 				money = date * 15000;
 				PayHistory pay_add = new PayHistory(userID, money, startDate);
-				int insertPay = userseat.addPay(conn, tmt, pay_add);
+				userseat.addPay(conn, tmt, pay_add);
 				
 				
 				UserSeatHistory addSeatDate = new UserSeatHistory(userID, startDate, endDate);
 				// 좌석이 있는지 확인(기존 등록되어있던 회원인지 확인)
-				if (userseat.selectSeat(conn, tmt, userID)) {
-					int updateSeat = userseat.updateSeat(conn, tmt, addSeatDate, userID);
+				if (UserPaySql.selectSeat(conn, tmt, userID)) {
+					userseat.updateSeat(conn, tmt, addSeatDate, userID);
 				} else {
-					int insertSeat = userseat.addSeat(conn, tmt, addSeatDate);
+					userseat.addSeat(conn, tmt, addSeatDate);
 				}
 				
 				
@@ -91,4 +92,5 @@ public class PayMain {
 		}
 	}
 }
+
 
